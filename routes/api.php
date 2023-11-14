fb29bb2e712f8aff58aa2dc93f9e23c1a6214ce0
ahsans1997 <ahsanulhasan599@gmail.com>
2023-11-14 14:26:13 +0600

    api with jwt

---
+++ routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+    Route::post('register', [AuthController::class, 'register']);
+});
+
+Route::middleware('auth:api')->group(function(){
+    Route::get('user/{id}', [UserController::class, 'user'])->name('user');
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('roles', RoleController::class);
+});
